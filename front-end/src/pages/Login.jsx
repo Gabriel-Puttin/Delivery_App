0f7c01b8f27c8bbb50c894d12b141a995111d99c
Gabriel Puttin <gabrielputtin@hotmail.com>
2023-02-27 19:13:50 -0300

    Feat: Validations for email and password

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Login() {
+  const [login, setLogin] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    const { email, password } = login;
+    const validateEmail = /\S+@\S+\.\S+/;
+    const validatePassword = 6;
+    const disabled = true;
+    if (password.length >= validatePassword && validateEmail.test(email)) {
+      setIsDisabled(!disabled);
+    } else {
+      setIsDisabled(disabled);
+    }
+  }, [login, isDisabled]);
+
+  const handleChange = (event) => {
+    const { target } = event;
+    setLogin({ ...login, [target.name]: target.value });
+  };
+
   return (
     <section>
       <form>
@@ -9,16 +33,21 @@ export default function Login() {
           type="email"
           placeholder="Email"
           name="email"
+          onChange={ handleChange }
+          value={ login.email }
         />
         <input
           data-testid="common_login__input-password"
           type="password"
           placeholder="Password"
           name="password"
+          onChange={ handleChange }
+          value={ login.password }
         />
         <button
           data-testid="common_login__button-login"
           type="submit"
+          disabled={ isDisabled }
         >
           LOGIN
         </button>
