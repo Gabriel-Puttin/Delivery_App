46fca64a924770ccd4b63fb5f28ae8e17a2d878b
tormelo <23294699+tormelo@users.noreply.github.com>
2023-02-28 10:38:37 -0300

    feat: add Register page elements

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -2,8 +2,40 @@ import React from 'react';
 
 export default function Register() {
   return (
-    <div>
-      Página de registro
-    </div>
+    <section>
+      <form>
+        <input
+          data-testid="common_register__input-name"
+          type="text"
+          placeholder="Nome"
+          name="name"
+        />
+        <input
+          data-testid="common_register__input-email"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <input
+          data-testid="common_register__input-password"
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+        <button
+          data-testid="common_register__button-register"
+          type="submit"
+        >
+          CADASTRAR
+        </button>
+      </form>
+
+      <span
+        data-testid="common_register__element-invalid-register"
+      >
+        Mensagem de erro
+      </span>
+
+    </section>
   );
 }
