89483e369682b933f7011a0f2728cc40cb5710a2
tormelo <23294699+tormelo@users.noreply.github.com>
2023-02-28 10:51:51 -0300

    feat: add register form input validation

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Register() {
+  const [registerForm, setRegisterForm] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    const { name, email, password } = registerForm;
+    const validateEmail = /\S+@\S+\.\S+/;
+    const minNameLength = 12;
+    const minPasswordLength = 6;
+    if (name.length >= minNameLength
+       && password.length >= minPasswordLength
+       && validateEmail.test(email)) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [registerForm, isDisabled]);
+
+  const handleChange = (event) => {
+    const { target } = event;
+    setRegisterForm({ ...registerForm, [target.name]: target.value });
+  };
+
   return (
     <section>
       <form>
@@ -9,22 +36,29 @@ export default function Register() {
           type="text"
           placeholder="Nome"
           name="name"
+          onChange={ handleChange }
+          value={ registerForm.name }
         />
         <input
           data-testid="common_register__input-email"
           type="email"
           placeholder="Email"
           name="email"
+          onChange={ handleChange }
+          value={ registerForm.email }
         />
         <input
           data-testid="common_register__input-password"
           type="password"
           placeholder="Password"
           name="password"
+          onChange={ handleChange }
+          value={ registerForm.password }
         />
         <button
           data-testid="common_register__button-register"
           type="submit"
+          disabled={ isDisabled }
         >
           CADASTRAR
         </button>
