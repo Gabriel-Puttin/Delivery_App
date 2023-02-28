25eae070a937d91851f66627a805b59e485cfb3e
tormelo <23294699+tormelo@users.noreply.github.com>
2023-02-28 07:53:13 -0300

    feat: config server cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,6 +3,13 @@ const { loginRoute } = require('../Routes/Login.Route');
 const { errorMiddleware } = require('../middlewares/errorMiddleware');
 
 const app = express();
+app.use(express.json());
+
+app.use(function(_req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRoute);
