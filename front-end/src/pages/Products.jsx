c44715f382429779238defb15868e959442536d5
tormelo <23294699+tormelo@users.noreply.github.com>
2023-03-07 12:19:38 -0300

    refactor: update Products page

--- front-end/src/pages/Products.jsx
+++ front-end/src/pages/Products.jsx
@@ -3,37 +3,33 @@ import { useNavigate } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import { requestData } from '../services/requests';
 
-const ONE = -1;
+const REMOVE_AMOUNT = -1;
+const ADD_AMOUNT = 1;
 
 function Products() {
   const navigate = useNavigate();
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const allProducts = await requestData('/products');
-      const newCart = allProducts.map(({ name, id, price }) => ({
-        name,
-        id,
-        price,
+      const productsData = await requestData('/products');
+      setProducts(productsData.map((product) => ({
+        ...product,
         quantity: 0,
-      }));
-      setCart(newCart);
-      setProducts(allProducts);
+      })));
     };
     fetchProducts();
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('carrinho', JSON.stringify(cart));
-    const total = cart.reduce((acc, curr) => acc + (curr.price * curr.quantity), 0);
+    localStorage.setItem('carrinho', JSON.stringify(products));
+    const total = products.reduce((acc, curr) => acc + (curr.price * curr.quantity), 0);
     setTotalPrice(total);
-  }, [cart]);
+  }, [products]);
 
   const handleClick = (id, amount) => {
-    setCart(cart.map((item) => {
+    setProducts((p) => p.map((item) => {
       const value = item.quantity + amount;
       if (item.id === id) {
         return { ...item, quantity: value < 0 ? 0 : value };
@@ -45,10 +41,12 @@ function Products() {
   const handleChange = (event) => {
     const { target } = event;
     const { id, value } = target;
-    const numberCart = Number(value) < 0 ? 0 : Number(value);
-    setCart(cart.map((item) => {
+    setProducts((p) => p.map((item) => {
       if (item.id === Number(id)) {
-        return { ...item, quantity: numberCart };
+        return {
+          ...item,
+          quantity: Number(value) < 0 ? 0 : Number(value),
+        };
       }
       return item;
     }));
@@ -62,7 +60,7 @@ function Products() {
     <section>
       <NavBar />
       {products.map((product, index) => (
-        <section key={ product.id }>
+        <div key={ `product-${product.id}` }>
           <h4
             data-testid={ `customer_products__element-card-price-${product.id}` }
           >
@@ -81,7 +79,7 @@ function Products() {
           <button
             data-testid={ `customer_products__button-card-rm-item-${product.id}` }
             type="button"
-            onClick={ () => handleClick(product.id, ONE) }
+            onClick={ () => handleClick(product.id, REMOVE_AMOUNT) }
           >
             -
           </button>
@@ -89,18 +87,19 @@ function Products() {
             data-testid={ `customer_products__input-card-quantity-${product.id}` }
             id={ product.id }
             type="number"
-            value={ cart[index].quantity }
+            value={ products[index].quantity }
             onChange={ handleChange }
             min="0"
           />
           <button
             data-testid={ `customer_products__button-card-add-item-${product.id}` }
             type="button"
-            onClick={ () => handleClick(product.id, 1) }
+            onClick={ () => handleClick(product.id, ADD_AMOUNT) }
           >
             +
           </button>
-        </section>))}
+        </div>
+      ))}
       <button
         data-testid="customer_products__button-cart"
         type="button"
@@ -111,7 +110,6 @@ function Products() {
           data-testid="customer_products__checkout-bottom-value"
         >
           { totalPrice.toFixed(2).toString().replace('.', ',') }
-
         </span>
       </button>
     </section>
