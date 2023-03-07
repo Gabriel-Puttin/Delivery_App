57b1064d78aa3476f9860cb5e0fd1b1ed06b5213
tormelo <23294699+tormelo@users.noreply.github.com>
2023-03-07 10:54:32 -0300

    refactor: update Orders pages

--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,51 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import NavBar from '../components/NavBar';
-import { requestData } from '../services/requests';
+import OrderList from '../components/Orders/OrderList';
 
 function Orders() {
-  const [sales, setSales] = useState([]);
-
-  useEffect(() => {
-    const fetchOrders = async () => {
-      const userSales = await requestData('/sales');
-      setSales(userSales);
-    };
-    fetchOrders();
-  }, []);
-
   return (
-    <div>
+    <section>
       <NavBar />
-      {sales.map((sale, index) => (
-        <Link to={ `/customer/orders/${sale.id}` } key={ index }>
-          <p
-            data-testid={ `customer_orders__element-order-id-${sale.id}` }
-          >
-            { index + 1 }
-          </p>
-          <h3
-            data-testid={ `customer_orders__element-delivery-status-${sale.id}` }
-          >
-            { sale.status }
-
-          </h3>
-          <p
-            data-testid={ `customer_orders__element-order-date-${sale.id}` }
-          >
-            { moment(sale.saleDate).format('DD/MM/YYYY') }
-
-          </p>
-          <p
-            data-testid={ `customer_orders__element-card-price-${sale.id}` }
-          >
-            {sale.totalPrice.replace('.', ',')}
-
-          </p>
-        </Link>
-      ))}
-    </div>
+      <OrderList userRole="customer" />
+    </section>
   );
 }
 
